602b4d8bcd268b0b9f98d1ff1cf27c8f7dfc07c9
Swarup Lamichhane <lswarup1234@gmail.com>
2024-06-11 15:27:21 +0545

    feat: Add basic server setup and endpoint for root URL

--- api-gateway/src/app.ts
+++ api-gateway/src/app.ts
@@ -4,3 +4,11 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server running on port ${process.env.PORT}`);
+});
